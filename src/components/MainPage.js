f8e27b4fbca46facb0d7f2a31fea3cd11073232a
rdick <62073529+rdick@users.noreply.github.com>
2022-01-10 00:45:50 -0800

    added final icon

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { AiFillDownCircle } from 'react-icons/ai';
 export const MainPage = () => {
 	return (
 		<>
 			<div className="welcome-container">
-				<h1 className="luxor-auctions">Benjamin Michael Garner</h1>
+				<h1 className="luxor-auctions">
+					Benjamin Michael Garner <AiFillDownCircle size={60} />
+				</h1>
 			</div>
 			<div class="fill">
 				<img src="IMG_0360.jpg" alt="" />
