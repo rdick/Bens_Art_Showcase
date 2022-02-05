7ac9d29f1cbeb7cdf236aeb7f559679647b0175c
rdick <62073529+rdick@users.noreply.github.com>
2022-02-04 19:27:32 -0700

    added information about paintings

--- src/index.js
+++ src/index.js
@@ -5,24 +5,9 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastProvider } from 'react-toast-notifications';
 
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnmount: true,
-			refetchOnWindowFocus: false,
-			refetchOnReconnect: false,
-			retry: false,
-		},
-	},
-});
-
 ReactDOM.render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<ToastProvider autoDismiss autoDismissTimeout={2000} placement="bottom-center">
-				<App />
-			</ToastProvider>
-		</QueryClientProvider>
+		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
