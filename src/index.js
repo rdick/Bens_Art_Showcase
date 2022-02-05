fa2aec601c7ea294e686adc6558b0f71b65995d5
rdick <62073529+rdick@users.noreply.github.com>
2022-02-04 20:05:30 -0700

    fixed bug

--- src/index.js
+++ src/index.js
@@ -5,9 +5,24 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastProvider } from 'react-toast-notifications';
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnmount: true,
+			refetchOnWindowFocus: false,
+			refetchOnReconnect: false,
+			retry: false,
+		},
+	},
+});
+
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<ToastProvider autoDismiss autoDismissTimeout={2000} placement="bottom-center">
+				<App />
+			</ToastProvider>
+		</QueryClientProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
